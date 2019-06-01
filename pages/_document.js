7a997b05a06d23540598b84fcafa232d3b5e9d2e
Daw-Chih Liou <dawochih.liou@gmail.com>
2019-06-01 16:01:25 +0200

    feat(favicon): add favicon for various platform

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,35 @@ class CustomDocument extends Document {
             content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
           />
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta
+            name="msapplication-TileImage"
+            content="/static/mstile-144x144.png"
+          />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/static/safari-pinned-tab.svg"
+            color="#fe6b8b"
+          />
         </Head>
         <body>
           <Main />
