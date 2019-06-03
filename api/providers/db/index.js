87d3e97d70a998abe9401391e9197ff7fcd5b962
Daw-Chih Liou <dawochih.liou@gmail.com>
2019-06-03 10:05:50 +0200

    fix(.env): fix environment variable in production

--- api/providers/db/index.js
+++ api/providers/db/index.js
@@ -3,8 +3,21 @@ const mongoose = require('mongoose');
 const connectDb = () => {
   const { MLAB_URL, MLAB_USERNAME, MLAB_PASSWORD } = process.env;
   const url = `mongodb://${MLAB_USERNAME}:${MLAB_PASSWORD}${MLAB_URL}`;
-  mongoose.Promise = global.Promise;
-  mongoose.connect(url, { useNewUrlParser: true });
+
+  console.log(
+    '------------test--------------\n',
+    MLAB_URL,
+    MLAB_USERNAME,
+    MLAB_PASSWORD,
+    '\n------------------------------'
+  );
+
+  try {
+    mongoose.Promise = global.Promise;
+    mongoose.connect(url, { useNewUrlParser: true });
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = connectDb;
