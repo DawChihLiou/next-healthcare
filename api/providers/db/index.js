fa20676194168502a9d59da5fe7e03385e42b5fe
Daw-Chih Liou <dawochih.liou@gmail.com>
2019-06-03 09:39:21 +0200

    fix: move .env to /api

--- api/providers/db/index.js
+++ api/providers/db/index.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const connectDb = () => {
   const { MLAB_URL, MLAB_USERNAME, MLAB_PASSWORD } = process.env;
   const url = `mongodb://${MLAB_USERNAME}:${MLAB_PASSWORD}${MLAB_URL}`;
-
   mongoose.Promise = global.Promise;
   mongoose.connect(url, { useNewUrlParser: true });
 };
